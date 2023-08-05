039ae457f93535d5b6f29b95772505f579698de8
Jonas Schernich <85169499+JonasSchernich@users.noreply.github.com>
2023-08-05 11:59:37 +0200

    Delete DataExplore.R

--- DataExplore.R
+++
@@ -1,2 +0,0 @@
-indices <- read.csv2("Indizes.csv")
-colnames(indices) <- c("date", "WORLD", "EM", "FTSEDIV", "FTSEPER", "FTSE", "EURODIV", "EUROPER", "EURO", "NASDAQ", "SP500", "DAX", "SPSMALL600VALUE", "HANGSENGDIV", "HANGSENGPER", "HANGSENG", "SP500VALUE", "SP500GROWTH")
